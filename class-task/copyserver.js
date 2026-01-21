bfbe2a7ecb3a0496f4e17c4f37ca315e790bac66
Ananya Chaurasia <ananyachaurasia74@gmail.com>
2026-01-21 16:21:18 +0530

    [Ananya] Add: Files added

---
+++ class-task/copyserver.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const fs = require("fs");
+
+const Stream = path.join(__dirname,"./stream.txt")
+
+const readStream  = fs.readStream(Stream,{
+    highWaterMark: 64*1024
+})
+
+
+readStream.on("data",(chunk)=>{
+    console.log(chunk.toString())
+})
+
+readStream.on("end",()=>{
+    console.log("Data Finshed")
+})
